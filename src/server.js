b3ff90fce9ec85298d80bd54d1e0c465fec46272
oktayudha05 <oktayudha05@gmail.com>
2022-06-16 12:15:12 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -3,8 +3,13 @@ import {routes} from './routes.js'
 
 const init = async () => {
     const server = Hapi.server({
-        port: 5000,
-        host: 'localhost'
+        port : 5000,
+        host : 'localhost',
+        routes : {
+            cors : {
+                origin : ['*']
+            }
+        }
     })
 
     server.route(routes)
